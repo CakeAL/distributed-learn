57129687094e324773b8f6b2a5fd61269e18a872
CakeAL <1742507190@qq.com>
2024-06-06 12:18:24 +0800

    3-4 服务发现2

--- registry/client.go
+++ registry/client.go
@@ -4,13 +4,23 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"sync"
 )
 
 func RegisterService(r Registration) error {
+	serviceUpdateURL, err := url.Parse(r.ServiceUpdateURL)
+	if err != nil {
+		return err
+	}
+	http.Handle(serviceUpdateURL.Path, &serviceUpdateHandler{})
+
 	buf := new(bytes.Buffer)
 	enc := json.NewEncoder(buf)
-	err := enc.Encode(r)
+	err = enc.Encode(r)
 	if err != nil {
 		return err
 	}
@@ -26,6 +36,24 @@ func RegisterService(r Registration) error {
 	}
 
 	return nil
+}
+
+type serviceUpdateHandler struct{}
+
+var _ http.Handler = (*serviceUpdateHandler)(nil)
+
+func (suh serviceUpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	dec := json.NewDecoder(r.Body)
+	var p patch
+	err := dec.Decode(&p)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadGateway)
+	}
 }
 
 func ShutdownService(url string) error {
@@ -44,4 +72,51 @@ func ShutdownService(url string) error {
 			"service responded with code %v", res.StatusCode)
 	}
 	return nil
+}
+
+type providers struct {
+	services map[ServiceName][]string // 对应多个URL都能提供该服务
+	mutex    *sync.RWMutex
+}
+
+func (p *providers) Update(pat patch) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	for _, patchEntry := range pat.Added {
+		if _, ok := p.services[patchEntry.Name]; !ok {
+			p.services[patchEntry.Name] = make([]string, 0)
+		}
+		p.services[patchEntry.Name] = append(p.services[patchEntry.Name],
+			patchEntry.URL)
+	}
+
+	for _, patchEntry := range pat.Removed {
+		if providerURLs, ok := p.services[patchEntry.Name]; ok {
+			for i := range providerURLs {
+				if providerURLs[i] == patchEntry.URL {
+					p.services[patchEntry.Name] = append(providerURLs[:i],
+						providerURLs[i+1:]...)
+				}
+			}
+		}
+	}
+}
+
+func (p providers) get(name ServiceName) (string, error) {
+	providers, ok := p.services[name]
+	if !ok {
+		return "", fmt.Errorf("No providers available for service %v", name)
+	}
+	idx := int(rand.Float32() * float32(len(providers)))
+	return providers[idx], nil
+}
+
+func GetProvider(name ServiceName) (string, error) {
+	return prov.get(name)
+}
+
+var prov = providers{
+	services: make(map[ServiceName][]string),
+	mutex:    new(sync.RWMutex),
 }
